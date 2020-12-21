bdf5d182e446380aefa6c16d8f5020a62b5e9085
Manuel Herrera <334509+manuelhe@users.noreply.github.com>
2020-12-20 22:13:20 -0500

    refactor(api): add types and fix method params

--- src/api/opportunities.ts
+++ src/api/opportunities.ts
@@ -1,14 +1,40 @@
-import { stringify, StringifiableRecord } from 'query-string';
+import { stringify } from 'query-string';
 import constants from '../constants';
 
-async function fetchOpportunities(queryParams?: StringifiableRecord, queryBody?: {}) {
+export type QueryParams = {
+  offset?: number,
+  size?: number,
+  aggregate?: boolean,
+  currency?: string,
+  page?: number,
+  periodicity?: string,
+  lang?: string,
+};
+export type QueryResponse = {
+  aggregators?: Record<string, any>,
+  results?: Record<string, any>[],
+  offset?: number,
+  size?: number,
+  total?: number
+};
+
+type QueryFunctionParams = {
+  queryParams: QueryParams,
+  queryBody: string;
+}
+interface CustomQueryFunctionContext {
+  queryKey: [key: string, params: QueryFunctionParams]
+}
+
+const fetchOpportunities = async ({ queryKey }: CustomQueryFunctionContext): Promise<QueryResponse> => {
+  const [key, { queryParams, queryBody }] = queryKey;
   const config = {
     method: 'POST',
     headers: { "Content-type": "application/json;charset=UTF-8" },
-    body: JSON.stringify(queryBody)
+    body: queryBody
   };
-  const querystring = queryParams ? `?${stringify(queryParams)}` : '';
-  const { ok, json } = await fetch(`${constants.endpoints.opportunities}${querystring}`, config);
+  const querystring = stringify(queryParams);
+  const { ok, json } = await fetch(`${constants.endpoints.opportunities}?${querystring}`, config);
 
   if (!ok) {
     throw new Error('Network unexpected error');
